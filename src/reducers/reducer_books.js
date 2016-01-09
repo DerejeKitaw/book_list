fb5482889693dce8ffca3d3b74161fc60e238392
Francisco De La Cruz <helloquico@gmail.com>
2016-01-09 12:00:16 -0500

    Reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'Eloquent JavaScript' },
+    { title: 'Programming JavaScript Applications' },
+    { title: 'Learn JavaScript Design Patterns' }
+  ];
+}
